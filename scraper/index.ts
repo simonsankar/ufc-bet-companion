47c183a1f4214ad02293049624da7960f3e87ad6
Simon Sankar <sjsankar10@gmail.com>
2022-07-31 11:55:20 -0400

    Improved scraper with id/outcomes

---
+++ scraper/index.ts
@@ -0,0 +1,148 @@
+import { load } from 'cheerio'
+
+type Fight = {
+  redCorner: {
+    firstName: string
+    lastName: string
+    rank: string
+    odds: number
+    outcome: string
+  }
+  blueCorner: {
+    firstName: string
+    lastName: string
+    rank: string
+    odds: number
+  }
+  bout: string
+}
+
+export type MainCard = {
+  id: string
+  title: string
+  timeStamp: number
+  fights: Fight[]
+}
+
+export const getMainCard = async (): Promise<MainCard> => {
+  const resp = await fetch('https://www.ufc.com/events')
+  const html = await resp.text()
+  const $ = load(html)
+
+  const nextEvent = $('.c-hero__actions')
+  const nextEventLink = nextEvent.find('a').first().attr()?.['href']
+  if (!nextEventLink) {
+    throw new Error('No next event link found')
+  }
+
+  const id = nextEventLink.replace('https://www.ufc.com/event/', '')
+  const eventResp = await fetch(nextEventLink)
+  // const eventResp = await fetch('https://www.ufc.com/event/ufc-276')
+  const eventHtml = await eventResp.text()
+  const $event = load(eventHtml)
+
+  const title = $event('div.c-hero__headline-prefix').first().text().trim()
+  const timeStamp = parseInt(
+    $event('div.c-hero__headline-suffix.tz-change-inner').first().attr()?.[
+      'data-timestamp'
+    ] || '0'
+  )
+
+  const mainCard = $event('ul.l-listing__group--bordered').first()
+  const mainCardList: Fight[] = mainCard
+    .find('li.l-listing__item')
+    .map((i, el) => {
+      const content = $event(el).find('.c-listing-fight__content').first()
+      // Red corner
+      const redCornerRank: string = content
+        .find(
+          '.js-listing-fight__corner-rank.c-listing-fight__corner-rank > span'
+        )
+        .first()
+        .text()
+        .trim()
+      const redCornerFirstName: string = content
+        .find('.c-listing-fight__corner-given-name')
+        .first()
+        .text()
+        .trim()
+      const redCornerLastName: string = content
+        .find('.c-listing-fight__corner-family-name')
+        .first()
+        .text()
+        .trim()
+      const redCornerOdds: number = parseInt(
+        content.find('span.c-listing-fight__odds-amount').first().text().trim()
+      )
+      const redOutcome: string = content
+        .find(
+          '.c-listing-fight__corner-body--red >.c-listing-fight__outcome-wrapper'
+        )
+        .first()
+        .text()
+        .trim()
+
+      // Red corner
+      const blueCornerRank: string = content
+        .find(
+          '.js-listing-fight__corner-rank.c-listing-fight__corner-rank > span'
+        )
+        .last()
+        .text()
+        .trim()
+      const blueCornerFirstName: string = content
+        .find('.c-listing-fight__corner-given-name')
+        .last()
+        .text()
+        .trim()
+      const blueCornerLastName: string = content
+        .find('.c-listing-fight__corner-family-name')
+        .last()
+        .text()
+        .trim()
+      const blueCornerOdds: number = parseInt(
+        content
+          .find('span.c-listing-fight__odds-amount')
+          .next()
+          .next()
+          .text()
+          .trim()
+      )
+      const blueOutcome: string = content
+        .find(
+          '.c-listing-fight__corner-body--blue >.c-listing-fight__outcome-wrapper'
+        )
+        .first()
+        .text()
+        .trim()
+
+      const bout: string = content.find('.c-listing-fight__class').text().trim()
+
+      return {
+        redCorner: {
+          firstName: redCornerFirstName,
+          lastName: redCornerLastName,
+          rank: redCornerRank,
+          odds: redCornerOdds,
+          outcome: redOutcome,
+        },
+        blueCorner: {
+          firstName: blueCornerFirstName,
+          lastName: blueCornerLastName,
+          rank: blueCornerRank,
+          odds: blueCornerOdds,
+          outcome: blueOutcome,
+        },
+
+        bout,
+      }
+    })
+    .get()
+
+  return {
+    id,
+    title,
+    timeStamp,
+    fights: mainCardList,
+  }
+}

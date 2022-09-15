8cd2d03b535d100f4ab41eadfb968f3571b12aed
Simon Sankar <sjsankar10@gmail.com>
2022-09-14 20:04:05 -0400

    Added bet accordion placeholder on fight

--- components/bout.tsx
+++ components/bout.tsx
@@ -12,6 +12,7 @@ import {
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
+  useColorMode,
 } from '@chakra-ui/react'
 import { Fight } from '../scraper'
 
@@ -192,16 +193,21 @@ const Odds: React.FC<OddsProps> = (props) => {
 }
 
 const Bet = () => {
+  const { colorMode } = useColorMode()
   return (
     <Accordion mt={2} width="100%" allowToggle>
       <AccordionItem border="none">
-        <AccordionButton>
+        <AccordionButton
+          _expanded={{
+            bg: colorMode === 'light' ? 'gray.100' : 'gray.700',
+          }}
+        >
           <Box flex="1" textAlign="left">
             Bets
           </Box>
           <AccordionIcon />
         </AccordionButton>
-        <AccordionPanel pb={4}>List of Bets</AccordionPanel>
+        <AccordionPanel pb={2}>List of Bets</AccordionPanel>
       </AccordionItem>
     </Accordion>
   )
